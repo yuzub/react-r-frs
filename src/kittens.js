e778392f65ea2d0f33088f0d6ec36c0337384316
Yuriy Zubaha <yuriy.zubaha@gmail.com>
2018-11-30 03:00:36 +0200

    kittens card done

---
+++ src/kittens.js
@@ -0,0 +1,602 @@
+export const kittens = [
+	{
+		"id": 1,
+		"name": "Nina",
+		"code": "IJM33",
+		"email": "eget@variuset.net"
+	},
+	{
+		"id": 2,
+		"name": "Bo",
+		"code": "MKR76",
+		"email": "odio@Duisdignissim.com"
+	},
+	{
+		"id": 3,
+		"name": "Jeanette",
+		"code": "HPE40",
+		"email": "blandit.congue.In@ipsumdolor.edu"
+	},
+	{
+		"id": 4,
+		"name": "Dacey",
+		"code": "XAC47",
+		"email": "tristique.ac@atlacus.com"
+	},
+	{
+		"id": 5,
+		"name": "Basil",
+		"code": "NOB32",
+		"email": "tincidunt@dignissim.ca"
+	},
+	{
+		"id": 6,
+		"name": "Samson",
+		"code": "IEI09",
+		"email": "ornare.facilisis@odiovelest.ca"
+	},
+	{
+		"id": 7,
+		"name": "Blythe",
+		"code": "UJD38",
+		"email": "nascetur.ridiculus@convallisconvallisdolor.org"
+	},
+	{
+		"id": 8,
+		"name": "Lucas",
+		"code": "SRP88",
+		"email": "blandit@acturpisegestas.org"
+	},
+	{
+		"id": 9,
+		"name": "Avram",
+		"code": "EPM82",
+		"email": "arcu.eu@vestibulum.com"
+	},
+	{
+		"id": 10,
+		"name": "Mallory",
+		"code": "OZD95",
+		"email": "pharetra.ut.pharetra@felispurusac.co.uk"
+	},
+	{
+		"id": 11,
+		"name": "Brent",
+		"code": "GLV03",
+		"email": "eget.varius@nec.co.uk"
+	},
+	{
+		"id": 12,
+		"name": "Beau",
+		"code": "MGA92",
+		"email": "auctor.Mauris.vel@dolorFusce.ca"
+	},
+	{
+		"id": 13,
+		"name": "Hayley",
+		"code": "TQC01",
+		"email": "euismod.est@accumsansed.com"
+	},
+	{
+		"id": 14,
+		"name": "Steven",
+		"code": "KXS39",
+		"email": "nec.enim@nonfeugiat.edu"
+	},
+	{
+		"id": 15,
+		"name": "Rhoda",
+		"code": "AUZ38",
+		"email": "aliquam.arcu.Aliquam@Donecelementumlorem.org"
+	},
+	{
+		"id": 16,
+		"name": "Todd",
+		"code": "NRI31",
+		"email": "auctor.odio.a@Namnullamagna.com"
+	},
+	{
+		"id": 17,
+		"name": "Cedric",
+		"code": "BPD49",
+		"email": "interdum.libero.dui@nectellusNunc.org"
+	},
+	{
+		"id": 18,
+		"name": "Angela",
+		"code": "YML87",
+		"email": "Sed.eu@mifringillami.co.uk"
+	},
+	{
+		"id": 19,
+		"name": "Jayme",
+		"code": "INI00",
+		"email": "Integer.in@risusvarius.net"
+	},
+	{
+		"id": 20,
+		"name": "Jessamine",
+		"code": "XZO67",
+		"email": "et.pede.Nunc@magnisdis.ca"
+	},
+	{
+		"id": 21,
+		"name": "Buffy",
+		"code": "YOL59",
+		"email": "enim@ridiculusmus.org"
+	},
+	{
+		"id": 22,
+		"name": "Macon",
+		"code": "JRZ41",
+		"email": "nonummy@Donecatarcu.org"
+	},
+	{
+		"id": 23,
+		"name": "Honorato",
+		"code": "SDH85",
+		"email": "rutrum.eu@magnis.co.uk"
+	},
+	{
+		"id": 24,
+		"name": "Talon",
+		"code": "PXI00",
+		"email": "tincidunt.orci.quis@enimnec.com"
+	},
+	{
+		"id": 25,
+		"name": "Ryder",
+		"code": "EDZ08",
+		"email": "tellus.lorem@egestasascelerisque.org"
+	},
+	{
+		"id": 26,
+		"name": "Dean",
+		"code": "CBO16",
+		"email": "per.inceptos.hymenaeos@auctornonfeugiat.org"
+	},
+	{
+		"id": 27,
+		"name": "Kylee",
+		"code": "LYA07",
+		"email": "Suspendisse@Fusce.co.uk"
+	},
+	{
+		"id": 28,
+		"name": "Oliver",
+		"code": "DVH40",
+		"email": "ullamcorper@mollis.co.uk"
+	},
+	{
+		"id": 29,
+		"name": "Chandler",
+		"code": "GZD79",
+		"email": "placerat.velit.Quisque@cursusa.net"
+	},
+	{
+		"id": 30,
+		"name": "Jada",
+		"code": "KCA91",
+		"email": "enim.diam.vel@Donec.net"
+	},
+	{
+		"id": 31,
+		"name": "Jane",
+		"code": "HLS29",
+		"email": "risus.Nulla.eget@ipsumleo.net"
+	},
+	{
+		"id": 32,
+		"name": "Tarik",
+		"code": "XIW04",
+		"email": "eget@scelerisquesed.net"
+	},
+	{
+		"id": 33,
+		"name": "Evangeline",
+		"code": "SKZ63",
+		"email": "gravida.sagittis@Etiamvestibulummassa.org"
+	},
+	{
+		"id": 34,
+		"name": "Rhonda",
+		"code": "YQQ47",
+		"email": "vulputate.posuere.vulputate@massa.org"
+	},
+	{
+		"id": 35,
+		"name": "Simone",
+		"code": "XEQ48",
+		"email": "urna.Ut.tincidunt@scelerisquescelerisquedui.edu"
+	},
+	{
+		"id": 36,
+		"name": "Jamalia",
+		"code": "AGJ51",
+		"email": "diam.Sed.diam@eratneque.com"
+	},
+	{
+		"id": 37,
+		"name": "Samuel",
+		"code": "KLD67",
+		"email": "lacus.Nulla.tincidunt@Phasellus.net"
+	},
+	{
+		"id": 38,
+		"name": "Stewart",
+		"code": "YQH69",
+		"email": "vulputate.ullamcorper.magna@diamluctus.com"
+	},
+	{
+		"id": 39,
+		"name": "Madonna",
+		"code": "VAB91",
+		"email": "euismod.est@ornareelitelit.com"
+	},
+	{
+		"id": 40,
+		"name": "Hall",
+		"code": "YYP20",
+		"email": "dui.Suspendisse@Donecnon.ca"
+	},
+	{
+		"id": 41,
+		"name": "Mona",
+		"code": "JGU20",
+		"email": "Nunc.lectus.pede@Lorem.edu"
+	},
+	{
+		"id": 42,
+		"name": "Joshua",
+		"code": "IDW25",
+		"email": "Duis.elementum@tellusimperdiet.edu"
+	},
+	{
+		"id": 43,
+		"name": "Hammett",
+		"code": "YSG72",
+		"email": "ultrices@Nam.org"
+	},
+	{
+		"id": 44,
+		"name": "May",
+		"code": "DWC06",
+		"email": "pede.malesuada.vel@auctorMauris.org"
+	},
+	{
+		"id": 45,
+		"name": "Malik",
+		"code": "UUR34",
+		"email": "tellus@sapienAenean.net"
+	},
+	{
+		"id": 46,
+		"name": "Melinda",
+		"code": "JKC31",
+		"email": "mus.Aenean.eget@non.com"
+	},
+	{
+		"id": 47,
+		"name": "Liberty",
+		"code": "RPA48",
+		"email": "elit@ornarelectusjusto.edu"
+	},
+	{
+		"id": 48,
+		"name": "Rebecca",
+		"code": "ODS49",
+		"email": "nisi.nibh@diameudolor.net"
+	},
+	{
+		"id": 49,
+		"name": "Keiko",
+		"code": "MYY91",
+		"email": "Suspendisse.eleifend.Cras@orciadipiscingnon.co.uk"
+	},
+	{
+		"id": 50,
+		"name": "Yasir",
+		"code": "TVV11",
+		"email": "tincidunt@miacmattis.edu"
+	},
+	{
+		"id": 51,
+		"name": "Xandra",
+		"code": "AZF20",
+		"email": "ac.fermentum.vel@urnaNuncquis.edu"
+	},
+	{
+		"id": 52,
+		"name": "Ian",
+		"code": "OSQ08",
+		"email": "semper.tellus@Pellentesquehabitantmorbi.org"
+	},
+	{
+		"id": 53,
+		"name": "Robert",
+		"code": "VXN33",
+		"email": "libero.Morbi.accumsan@metus.co.uk"
+	},
+	{
+		"id": 54,
+		"name": "Ray",
+		"code": "CSN74",
+		"email": "adipiscing.lobortis@atauctorullamcorper.co.uk"
+	},
+	{
+		"id": 55,
+		"name": "Halee",
+		"code": "JVI51",
+		"email": "vel.nisl@iaculisnec.org"
+	},
+	{
+		"id": 56,
+		"name": "Reuben",
+		"code": "UUO91",
+		"email": "penatibus@fringilla.org"
+	},
+	{
+		"id": 57,
+		"name": "Akeem",
+		"code": "ARW63",
+		"email": "pede.Nunc@Aenean.org"
+	},
+	{
+		"id": 58,
+		"name": "Kadeem",
+		"code": "ZQL66",
+		"email": "urna.Vivamus@disparturient.edu"
+	},
+	{
+		"id": 59,
+		"name": "Blythe",
+		"code": "LFW21",
+		"email": "lectus.justo@et.com"
+	},
+	{
+		"id": 60,
+		"name": "Mona",
+		"code": "TVJ06",
+		"email": "tincidunt@mauriserat.org"
+	},
+	{
+		"id": 61,
+		"name": "Reuben",
+		"code": "HVR24",
+		"email": "scelerisque.dui.Suspendisse@ut.com"
+	},
+	{
+		"id": 62,
+		"name": "Tasha",
+		"code": "AQK23",
+		"email": "nec.cursus@amet.org"
+	},
+	{
+		"id": 63,
+		"name": "Claire",
+		"code": "HJI90",
+		"email": "libero.Proin@CuraePhasellusornare.co.uk"
+	},
+	{
+		"id": 64,
+		"name": "Zephr",
+		"code": "WRM98",
+		"email": "Integer.urna@sociisnatoque.edu"
+	},
+	{
+		"id": 65,
+		"name": "Hilary",
+		"code": "ODO17",
+		"email": "ultrices.mauris.ipsum@pretium.ca"
+	},
+	{
+		"id": 66,
+		"name": "Chaim",
+		"code": "QHP85",
+		"email": "lobortis.augue.scelerisque@quamquisdiam.edu"
+	},
+	{
+		"id": 67,
+		"name": "Shay",
+		"code": "GUA84",
+		"email": "vel.arcu@dictumsapienAenean.com"
+	},
+	{
+		"id": 68,
+		"name": "Eaton",
+		"code": "LMA21",
+		"email": "eget.mollis.lectus@etultrices.edu"
+	},
+	{
+		"id": 69,
+		"name": "Chaim",
+		"code": "JSO49",
+		"email": "velit.eu@apurusDuis.ca"
+	},
+	{
+		"id": 70,
+		"name": "Cassandra",
+		"code": "TGO23",
+		"email": "ac.mi@Fuscefeugiat.net"
+	},
+	{
+		"id": 71,
+		"name": "Chelsea",
+		"code": "PAS64",
+		"email": "tellus.Phasellus@loremvehiculaet.co.uk"
+	},
+	{
+		"id": 72,
+		"name": "Kenneth",
+		"code": "MLP32",
+		"email": "dapibus.ligula@sapiencursusin.ca"
+	},
+	{
+		"id": 73,
+		"name": "Hayfa",
+		"code": "FTU67",
+		"email": "ante.ipsum.primis@idsapien.org"
+	},
+	{
+		"id": 74,
+		"name": "Ira",
+		"code": "NNE10",
+		"email": "sed.tortor.Integer@Cras.edu"
+	},
+	{
+		"id": 75,
+		"name": "Paloma",
+		"code": "TFV67",
+		"email": "Aenean.gravida@ligulatortor.net"
+	},
+	{
+		"id": 76,
+		"name": "Isadora",
+		"code": "YNM75",
+		"email": "Ut.semper@duiFuscealiquam.co.uk"
+	},
+	{
+		"id": 77,
+		"name": "Caleb",
+		"code": "DAQ81",
+		"email": "fermentum@sedest.co.uk"
+	},
+	{
+		"id": 78,
+		"name": "Nolan",
+		"code": "GCV61",
+		"email": "Suspendisse@enimMauris.co.uk"
+	},
+	{
+		"id": 79,
+		"name": "Aimee",
+		"code": "OGH76",
+		"email": "Cras@nonenimcommodo.ca"
+	},
+	{
+		"id": 80,
+		"name": "Madeson",
+		"code": "IVP40",
+		"email": "adipiscing.elit.Curabitur@pede.net"
+	},
+	{
+		"id": 81,
+		"name": "Kevin",
+		"code": "ZCA20",
+		"email": "magna.sed@ametfaucibus.ca"
+	},
+	{
+		"id": 82,
+		"name": "Kylan",
+		"code": "EZK85",
+		"email": "iaculis@purusinmolestie.com"
+	},
+	{
+		"id": 83,
+		"name": "Selma",
+		"code": "NJC31",
+		"email": "lacus.vestibulum.lorem@arcuiaculis.org"
+	},
+	{
+		"id": 84,
+		"name": "Todd",
+		"code": "UKJ15",
+		"email": "porttitor@risus.org"
+	},
+	{
+		"id": 85,
+		"name": "Bree",
+		"code": "DFU50",
+		"email": "magna.tellus.faucibus@sitametlorem.co.uk"
+	},
+	{
+		"id": 86,
+		"name": "Isaiah",
+		"code": "TYB85",
+		"email": "vulputate.dui@necurna.net"
+	},
+	{
+		"id": 87,
+		"name": "Sylvester",
+		"code": "PAO61",
+		"email": "quis@lectusNullamsuscipit.com"
+	},
+	{
+		"id": 88,
+		"name": "Chadwick",
+		"code": "UFX49",
+		"email": "ut@infaucibusorci.org"
+	},
+	{
+		"id": 89,
+		"name": "Abel",
+		"code": "PZN72",
+		"email": "auctor@etcommodoat.edu"
+	},
+	{
+		"id": 90,
+		"name": "Kaden",
+		"code": "FIE41",
+		"email": "in@pulvinararcu.ca"
+	},
+	{
+		"id": 91,
+		"name": "Ira",
+		"code": "ZIS90",
+		"email": "est.arcu@accumsanconvallisante.com"
+	},
+	{
+		"id": 92,
+		"name": "Kameko",
+		"code": "JPN73",
+		"email": "Quisque@tempus.co.uk"
+	},
+	{
+		"id": 93,
+		"name": "Tashya",
+		"code": "CHH68",
+		"email": "augue.Sed@egestas.org"
+	},
+	{
+		"id": 94,
+		"name": "Sage",
+		"code": "NHP05",
+		"email": "lorem@necmollisvitae.edu"
+	},
+	{
+		"id": 95,
+		"name": "Hector",
+		"code": "MFL74",
+		"email": "In.tincidunt@Curae.ca"
+	},
+	{
+		"id": 96,
+		"name": "Bryar",
+		"code": "PXF82",
+		"email": "molestie.tellus@velarcu.org"
+	},
+	{
+		"id": 97,
+		"name": "Shannon",
+		"code": "RDS48",
+		"email": "Curae.Phasellus.ornare@maurisa.net"
+	},
+	{
+		"id": 98,
+		"name": "Zenaida",
+		"code": "YRZ78",
+		"email": "libero@nec.org"
+	},
+	{
+		"id": 99,
+		"name": "Samson",
+		"code": "TAG97",
+		"email": "tristique@lectuspede.co.uk"
+	},
+	{
+		"id": 100,
+		"name": "Rajah",
+		"code": "COH47",
+		"email": "Morbi@ac.com"
+	}
+];
